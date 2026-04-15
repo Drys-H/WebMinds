d59c36e11f4e4af0f0b84935de85b19ae434781c
Ross <msbalimezov@gmail.com>
2026-04-15 23:59:40 +0100

    Adding RecipeService interface

---
+++ backend/src/main/java/com/recipeapp/backend/service/RecipeService.java
@@ -0,0 +1,15 @@
+package com.recipeapp.backend.service;
+
+import com.recipeapp.backend.Recipe;
+import com.recipeapp.backend.dto.RecipeDTO;
+
+import java.util.List;
+
+public interface RecipeService {
+
+    List<RecipeDTO> getAllRecipes();
+    RecipeDTO getRecipeById(Long id);
+    RecipeDTO createRecipe(RecipeDTO recipeDTO);
+    List<RecipeDTO> searchRecipes(String keyword);
+
+}
