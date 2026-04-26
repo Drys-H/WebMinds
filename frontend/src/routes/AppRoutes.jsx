874ad92b952163b96bdf105523beab529ceba299
s3810826 <s3810826@lsbu.ac.uk>
2026-04-26 01:40:51 +0100

    Update AppRoutes.jsx

--- frontend/src/routes/AppRoutes.jsx
+++ frontend/src/routes/AppRoutes.jsx
@@ -8,7 +8,7 @@ import AllRecipes from "../pages/AllRecipes";
 import RecipeDetails from "../pages/RecipeDetails";
 import Categories from "../pages/Categories";
 import Community from "../pages/Community";
-import MyProfile from "../pages/MyProfile";
+import Profile from "../pages/Profile";
 import CreateRecipe from "../pages/CreateRecipe";
 import ShoppingList from "../pages/ShoppingList";
 
@@ -16,19 +16,25 @@ export default function AppRoutes() {
   return (
     <Routes>
 
-      {/* Layout wraps ALL pages */}
       <Route path="/" element={<Layout />}>
 
         <Route index element={<Home />} />
-        <Route path="sign-in" element={<SignIn />} />
+        <Route path="signin" element={<SignIn />} />
         <Route path="register" element={<Register />} />
+
         <Route path="recipes" element={<AllRecipes />} />
         <Route path="recipes/:recipeId" element={<RecipeDetails />} />
+
         <Route path="categories" element={<Categories />} />
         <Route path="community" element={<Community />} />
-        <Route path="profile" element={<MyProfile />} />
+
+        <Route path="profile" element={<Profile />} />
         <Route path="profile/shopping-list" element={<ShoppingList />} />
-        <Route path="recipes/create" element={<CreateRecipe />} />
+
+        <Route path="create" element={<CreateRecipe />} />
+
+        {/* fallback */}
+        <Route path="*" element={<Home />} />
 
       </Route>
 
