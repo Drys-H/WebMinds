32466300f8785f0217e226519a231874ddb8fd9b
s3810826 <s3810826@lsbu.ac.uk>
2026-04-26 01:36:07 +0100

    Update AppRoutes.jsx

--- frontend/src/routes/AppRoutes.jsx
+++ frontend/src/routes/AppRoutes.jsx
@@ -1,65 +1,36 @@
-import { Navigate, Route, Routes } from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
 import Layout from "../components/layout/Layout";
 
 import Home from "../pages/Home";
+import SignIn from "../pages/SignIn";
+import Register from "../pages/Register";
 import AllRecipes from "../pages/AllRecipes";
 import RecipeDetails from "../pages/RecipeDetails";
 import Categories from "../pages/Categories";
 import Community from "../pages/Community";
-
-import SignIn from "../pages/SignIn";
-import SignUp from "../pages/SignUp";
-import Profile from "../pages/Profile";
+import MyProfile from "../pages/MyProfile";
 import CreateRecipe from "../pages/CreateRecipe";
-
-import { isLoggedIn } from "../utils/auth";
-
-/* 🔒 PROTECTED ROUTE */
-const ProtectedRoute = ({ children }) => {
-  return isLoggedIn() ? children : <Navigate to="/signin" />;
-};
+import ShoppingList from "../pages/ShoppingList";
 
 export default function AppRoutes() {
   return (
     <Routes>
 
-      {/* MAIN APP (WITH NAVBAR LAYOUT) */}
+      {/* Layout wraps ALL pages */}
       <Route path="/" element={<Layout />}>
 
         <Route index element={<Home />} />
-
+        <Route path="sign-in" element={<SignIn />} />
+        <Route path="register" element={<Register />} />
         <Route path="recipes" element={<AllRecipes />} />
-        <Route path="recipes/:id" element={<RecipeDetails />} />
-
+        <Route path="recipes/:recipeId" element={<RecipeDetails />} />
         <Route path="categories" element={<Categories />} />
         <Route path="community" element={<Community />} />
+        <Route path="profile" element={<MyProfile />} />
+        <Route path="profile/shopping-list" element={<ShoppingList />} />
+        <Route path="recipes/create" element={<CreateRecipe />} />
 
-        {/*  PROTECTED INSIDE LAYOUT */}
-        <Route
-          path="profile"
-          element={
-            <ProtectedRoute>
-              <Profile />
-            </ProtectedRoute>
-          }
-        />
-
-        <Route
-          path="create"
-          element={
-            <ProtectedRoute>
-              <CreateRecipe />
-            </ProtectedRoute>
-          }
-        />
       </Route>
-
-      {/* AUTH PAGES (NO NAVBAR) */}
-      <Route path="/signin" element={<SignIn />} />
-      <Route path="/signup" element={<SignUp />} />
-
-      {/* FALLBACK */}
-      <Route path="*" element={<Navigate to="/" replace />} />
 
     </Routes>
   );
